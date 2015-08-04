ef64496565e3b543e7c8c5e05c18ecc525ce636c
Yanakorn K. <yanakorn.kij@outlook.com>
2015-08-05 01:03:29 +0700

    Update

--- nayoo_new/js/app/guest_record/guest_record.js
+++ nayoo_new/js/app/guest_record/guest_record.js
@@ -1,22 +1,5 @@
-app.controller('GuestRecordCtrl', ['$scope', '$http', 'guest_records', '$stateParams', function ($scope, $http, guest_records, $stateParams) {
-
-    $http.get('js/app/note/notes.json').then(function (resp) {
-        $scope.notes = resp.data.notes;
-        // set default note
-        $scope.note = $scope.notes[0];
-        $scope.notes[0].selected = true;
-    });
-
-    $scope.colors = ['primary', 'info', 'success', 'warning', 'danger', 'dark'];
-
-    $scope.fold = $stateParams.fold;
-    
-    guest_records.all().then(function (guest_records) {
-        $scope.guest_records = guest_records;
-    });
-
-
-
+app.controller('GuestRecordCtrl', function ($scope, $http, $modal) {
+  
 }]);
 
 
