106516b40a738d157b5737a825aa19a92d88af29
nuttaponsir <nuttapon@dits.co.th>
2015-08-06 00:04:24 +0700

    Create UI user

---
+++ nayoo_new/js/app/user/user.js
@@ -0,0 +1,23 @@
+app.controller('UserCtrl',  function($scope, $http, $modal ) { 
+     
+  $scope.ShowDlg =function(size)
+  {  
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size 
+      }); 
+  }; 
+      
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance) { 
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
